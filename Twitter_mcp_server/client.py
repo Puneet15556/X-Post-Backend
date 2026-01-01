3eed8d2ac9527cf791764f580c5679149dd46207
PUNEET RANJAN <puneet.22jgm@gmail.com>
2026-01-02 00:47:14 +0530

    Fix MCP server path using dynamic os.path

--- Twitter_mcp_server/client.py
+++ Twitter_mcp_server/client.py
@@ -1,10 +1,19 @@
+import os
 from langchain_mcp_adapters.client import MultiServerMCPClient
 
+
+
 async def create_client():
+    
+    
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+    
+    mcp_server_path = os.path.join(base_path, "twitter_mcp_server4.py")
     return MultiServerMCPClient({
         "twitter": {
             "command": "python",
-            "args": [r"C:\Users\PUNEET\Desktop\DESKTOP\Projects\Monolithic_Generator\Twitter_mcp_server\twitter_mcp_server4.py"],
+            "args": [mcp_server_path],
             "transport": "stdio"
         }
     })
